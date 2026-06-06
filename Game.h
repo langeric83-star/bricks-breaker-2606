f4cbace1c3add332de1c9bb4ad599e28dbca2f93
langeric83-star <langeric83@gmail.com>
2026-06-06 18:52:53 -0500

    brick obj into vector of obj by val

--- Game.h
+++ Game.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Box.h"
 #include "Ball.h"
+#include <vector>
 
 class Game
 {
@@ -8,7 +9,8 @@ class Game
 	Box paddle;
 
 	// TODO #1 - Instead of storing 1 brick, store a vector of bricks (by value)
-	Box brick;
+	//Box brick;
+	std::vector<Box> bricks; 
 
 public:
 	Game();
